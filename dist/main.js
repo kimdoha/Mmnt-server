aca5fd6c5b91fbad37b43e76fbd85850289aae43
kimdoha <kkhhjj3393@gmail.com>
2022-06-18 17:46:18 +0900

    dev, prod 개발 환경 분리

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const port = process.env.NODE_ENV === 'prod' ? 3000 : 3001;
+    await app.listen(port);
+    console.log('port', port);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.NODE_ENV === 'prod' ? 3000 : 3001;
+  await app.listen(port);
 }
 bootstrap();
