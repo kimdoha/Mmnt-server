f61bfcb74a97976836caefd2651ce499dd76fa4b
kimdoha <kkhhjj3393@gmail.com>
2022-07-04 16:10:05 +0900

    Chore: module 정리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { StatusCodes } from 'http-status-codes';
 import { SuccessReponse } from 'src/helpers/SuccessReponse';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/common/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
-import { CreateAuthorizedCodeResDto } from './dtos/create-authorized-code-res.dto';
-import { FindAuthorizedUserDto } from './dtos/find-authorized-user.dto';
+import { CreateAuthorizedCodeResponseDto } from '../common/response/auth/create.authorized-code.response.dto';
+import { FindAuthorizedUserDto } from './dtos/find.authorized-user.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('')
-    @Serialize(CreateAuthorizedCodeResDto)
+    @Serialize(CreateAuthorizedCodeResponseDto)
     async certificateUser(@Body() body: CreateUserDto, @Res() res) {
         const responseData = await this.authService.createAuthorizedCode(body.phone);
         return res
