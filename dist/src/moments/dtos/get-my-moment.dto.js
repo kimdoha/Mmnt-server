e6d6a13f50699411d9f375e8429fc98ec70a2030
kimdoha <kkhhjj3393@gmail.com>
2022-07-14 04:55:35 +0900

    Chore: build 정리

--- dist/src/moments/dtos/get-my-moment.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMyMoments = void 0;
-class getMyMoments {
-}
-exports.getMyMoments = getMyMoments;
-//# sourceMappingURL=get-my-moment.dto.js.map
